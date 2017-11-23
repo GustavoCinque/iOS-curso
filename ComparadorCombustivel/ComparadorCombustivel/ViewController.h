cdc4fd1c88f5abeb0328a3d285ae2cef24f1c820
Cast Group <castgroup@CastGroup.local>
2017-11-23 19:07:39 -0200

    Desenvolvimento comparacao combustivel

---
+++ ComparadorCombustivel/ComparadorCombustivel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComparadorCombustivel
+//
+//  Created by Cast Group on 23/11/17.
+//  Copyright © 2017 Cast Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate>
+
+
+@end
+
---
+++ ComparadorCombustivel/ComparadorCombustivel/ViewController.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  ComparadorCombustivel
+//
+//  Created by Cast Group on 23/11/17.
+//  Copyright © 2017 Cast Group. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (){
+    UITextField *_textFieldSelecionado;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *precoGasolina;
+@property (weak, nonatomic) IBOutlet UITextField *precoAlcool;
+@property (weak, nonatomic) IBOutlet UILabel *labelResultado;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)calcular:(id)sender {
+    
+    CGFloat precoAlcool = self.precoAlcool.text.floatValue;
+    CGFloat precoGasolina = self.precoGasolina.text.floatValue;
+    
+    CGFloat resultado = precoAlcool/precoGasolina;
+    
+    if(resultado >= 0.7) {
+        self.labelResultado.text = @"Melhor utilizar Gasolina";
+    } else {
+        self.labelResultado.text = @"Melhor utilizar Alcool";
+    }
+    
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    _textFieldSelecionado = textField;
+    return YES;
+}
+
+-	(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [_textFieldSelecionado resignFirstResponder];
+}
+
+@end
